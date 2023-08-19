225075adb5491979eed65b6d752ce816e1834d79
Leon <Leon@host.docker.internal>
2023-08-19 14:14:17 -0300

    PARTE 3

--- TDD-Atividade-Pratica/src/main/java/ifmg/tdd/atividade/pratica/Dollar.java
+++ TDD-Atividade-Pratica/src/main/java/ifmg/tdd/atividade/pratica/Dollar.java
@@ -9,62 +9,38 @@ package ifmg.tdd.atividade.pratica;
  * @author Leon
  */
 class Dollar {
-   private amount;
-   Dollar(int amount) {
-      this.amount= amount;
-   }
-   void times(int multiplier) {
-      amount= amount * multiplier;
-   }
-   Dollar times(int multiplier) {
-      return new Dollar(amount * multiplier);
-   }
-   public boolean equals(Object object)  {
-   Dollar dollar = (Dollar) object;
-   return amount == dollar.amount;
-}
-}	
-public void testMultiplication() {
-   Dollar five = new Dollar(5);
-   assertEquals(new Dollar(10), five.times(2));
-   assertEquals(new Dollar(15), five.times(3));
-}
 
-public void testEquality() {
-   assertTrue(new Dollar(5).equals(new Dollar(5)));
-   assertFalse(new Dollar(5).equals(new Dollar(6)));
-}
+    private amount ;
 
-public void testFrancMultiplication() {
-   Franc five = new Franc(5);
-   assertEquals(new Franc(10), five.times(2));
-   assertEquals(new Franc(15), five.times(3));
-}
+    Dollar(int amount) {
+        this.amount = amount;
+    }
 
-class Franc {   
-   private int amount;					
-   Franc(int amount) {      
-      this.amount= amount;
-    }					
-    Franc times(int multiplier)  {      
-       return new Franc(amount * multiplier);					
-    }   
-    public boolean equals(Object object) {					
-       Franc franc = (Franc) object;      
-       return amount == franc.amount;					
-     }					
+    void times(int multiplier) {
+        amount = amount * multiplier;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
+    String currency() {
+       return "USD";
+    }
+    
 }
-class Money  {
-   protected int amount;
-   
-   public boolean equals(Object object)  {
-      Money money = (Money) object;
-      return amount == money.amount;
-   }   
-}
-public void testEquality() {
-   assertTrue(new Dollar(5).equals(new Dollar(5)));
-   assertFalse(new Dollar(5).equals(new Dollar(6)));
-   assertTrue(new Franc(5).equals(new Franc(5)));
-   assertFalse(new Franc(5).equals(new Franc(6)));
+class Dollar extends Money {
+   private String currency;
+	
+Dollar(int amount, String currency)  {
+      super(amount, currency);
+   }
+	
+   Money times(int multiplier)  {
+      return Money.dollar(amount * multiplier);
+   }
 }
