2c2161690227cf928ef1b89298a6185a56d59c2e
Leon <Leon@host.docker.internal>
2023-08-19 14:19:16 -0300

    PARTE 4

--- TDD-Atividade-Pratica/src/main/java/ifmg/tdd/atividade/pratica/test.java
+++ TDD-Atividade-Pratica/src/main/java/ifmg/tdd/atividade/pratica/test.java
@@ -16,13 +16,15 @@ public class test {
 }
 
 public void testEquality() {
-   assertTrue(new Dollar(5).equals(new Dollar(5)));
-   assertFalse(new Dollar(5).equals(new Dollar(6)));
+       assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
+   assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+   assertFalse(Money.franc(5).equals(Money.dollar(5)));
+   assertFalse(new Franc(5).equals(new Franc(6)));
+
 }
 
-public void testFrancMultiplication() {
-   Franc five = new Franc(5);
-   assertEquals(new Franc(10), five.times(2));
-   assertEquals(new Franc(15), five.times(3));
 }
+public void testCurrency() {
+   assertEquals("USD", Money.dollar(1).currency());
+   assertEquals("CHF", Money.franc(1).currency());
 }
